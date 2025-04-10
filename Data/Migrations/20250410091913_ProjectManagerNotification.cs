94f0398ab82c4e96177a4cb5c08064d9fe90d67c
Emil Begovic <emil@begovic.se>
2025-04-10 14:53:57 +0200

    changes

---
+++ Data/Migrations/20250410091913_ProjectManagerNotification.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProjectManagerNotification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ForProjectManagersOnly",
+                table: "Notifications",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { new Guid("02711297-d556-4867-99a0-281eca550915"), null, "Project Manager", "PROJECT MANAGER" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("02711297-d556-4867-99a0-281eca550915"));
+
+            migrationBuilder.DropColumn(
+                name: "ForProjectManagersOnly",
+                table: "Notifications");
+        }
+    }
+}
